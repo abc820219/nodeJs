bc4423813a8733c823cdc0166fa36ae24da6d4d6
jason <z27089433@gmail.com>
2019-09-24 12:14:03 +0800

    add

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 //引入express應用程式
-var express = require('express')
+const express = require('express')
 const url = require('url');
 const bodyParser = require('body-parser');
+const multer = require('multer');
+const upload = multer({ dest: 'tmp_uploads' });
+const fs = require('fs');
 
 //啟動
 var app = express();
@@ -9,9 +12,10 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.set('view engine', 'ejs');
 
-//啟動靜態資料夾
+//middle ware 啟動靜態資料夾 & 轉譯 & 轉譯JSON
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 
 app.get('/', function (req, res) {//根目錄
@@ -19,7 +23,7 @@ app.get('/', function (req, res) {//根目錄
 })
 
 
-//設定路由1號 2號
+//設定路由
 app.get('/0920sale', function (req, res) {
     const data = require(__dirname + './../data/0920sale.json');
     res.render('0920sale', {
@@ -36,14 +40,26 @@ app.get('/try-qs', (req, res) => {
     });
 });
 
-
 app.get('/try-post-form', (req, res) => {
     res.render('try-post-form');
 });
-app.post('/try-post-form', urlencodedParser, (req, res) => {
+
+app.post('/try-post-form', (req, res) => {
     res.render('try-post-form', req.body);
     console.log(req.body);
-    // res.send(JSON.stringify(req.body));
+    res.send(JSON.stringify(req.body));
+});
+
+app.get('/try-post-form2', (req, res) => {
+    res.send('get try-post-form');
+
+});
+app.post('/try-post-form2', (req, res) => {
+    res.send(req.body);
+});
+
+app.put('/try-post-form2', (req, res) => {
+    res.send('put try-post-form');
 });
 
 
@@ -51,12 +67,38 @@ app.get('/get2', function (req, res) {
     res.send('hello2');
 });
 
+app.post('/upload', upload.single('avatar'), (req, res) => {//單張圖片上傳
+    console.log(req.file);
+    // res.send('yes');
+    if (req.file && req.file.originalname) {
+        switch (req.file.mimetype) {
+            case "image/jpg":
+            case "image/png":
+            case "image/jpeg":
+                fs.createReadStream(req.file.path)//讀檔案
+                    .pipe(//串進去
+                        fs.createReadStream('./public/img/' + req.file.originalname)//寫檔案
+                    );
+                res.send('good boy');
+                break;
+            default:
+                return res.send('bad boy')
+        }
+    } else {
+        return res.send('bad bad boy')
+    }
+});
+
+
+
 //沒有別的路由啟動時啟動這個
 app.use((req, res) => {
     res.type('text/plain');
     res.status(404);
-    res.send('404');
+    res.send(`404`);
 });
+
+
 
 //給一個空間3000不能重複啟動
 app.listen(3000, function () {
